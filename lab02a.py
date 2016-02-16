7540e7b18fc8c879967996b7b290a29c3ef632fb
Andrew Howard <andrew.howard@rackspace.com>
2016-02-16 13:48:45 -0600

    Add lab02a and sample text

---
+++ lab02a.py
@@ -0,0 +1,15 @@
+import math
+
+myFile = open("alice_in_wonderland.dat", "r")
+data = myFile.read()
+
+countTotal = 0
+countE = 0
+
+for char in data:
+  countTotal += 1
+  if char in "eE":
+    countE += 1
+print "Total e's:   ",countE
+print "Total chars: ",countTotal
+print "Percentage of 'eE': ",round(float(countE*100)/countTotal,2)
