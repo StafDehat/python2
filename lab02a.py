e75b84c7697a06caa3eaf66f4c6fb9ca8f31c064
Andrew Howard <andrew.howard@rackspace.com>
2016-02-16 14:10:35 -0600

    Whoops, only letters count toward total

--- lab02a.py
+++ lab02a.py
@@ -1,4 +1,5 @@
 import math
+import string
 
 myFile = open("alice_in_wonderland.dat", "r")
 data = myFile.read()
@@ -7,9 +8,11 @@ countTotal = 0
 countE = 0
 
 for char in data:
-  countTotal += 1
-  if char in "eE":
-    countE += 1
+  if char in string.letters:
+    countTotal += 1
+    if char in "eE":
+      countE += 1
+
 print "Total e's:   ",countE
 print "Total chars: ",countTotal
 print "Percentage of 'eE': ",round(float(countE*100)/countTotal,2)
