bca9a4515896f1c0d16f5e897d64e8b4c37e9c9c
Andrew Howard <andrew.howard@rackspace.com>
2016-02-16 15:48:26 -0600

    Revise lab02a

--- lab02a.py
+++ lab02a.py
@@ -6,13 +6,12 @@ myFile = open("alice_in_wonderland.dat", "r")
 data = myFile.read()
 
 countTotal = 0
-countE = 0
 
 for char in data:
-  if char in string.letters:
+  if char.isalpha():
     countTotal += 1
-    if char in "eE":
-      countE += 1
+
+countE = data.count('e')+data.count("e")
 
 print "Total e's:   ",countE
 print "Total chars: ",countTotal
