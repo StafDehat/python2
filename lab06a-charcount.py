4a245855459e1b2453e47a61862a63a010fbe226
Andrew Howard <andrew.howard@rackspace.com>
2016-02-18 12:26:30 -0600

    Pretty formatting

--- lab06a-charcount.py
+++ lab06a-charcount.py
@@ -6,13 +6,31 @@ import sys
 def usage():
   print "Usage: %s filename" % (sys.argv[0])
 
+def maxLen(iterable):
+  longest = 0
+  for x in iterable:
+    if len(str(x)) > longest:
+      longest = len(str(x))
+  return longest
+
 def printDict(data, lineLen):
   lineNum = 0
+  valLen = maxLen(data.values())
+  keyLen = maxLen(data.keys())
+  print "Max value length:",valLen
+  print "Max key length:  ",keyLen
+  sortedData = sorted(zip(data.values(),data.keys()), reverse=True)
   while True:
-    if lineNum*lineLen > len(hist):
-      break
-    print zip(  hist.keys()[ lineLen*lineNum : lineLen*(lineNum+1) ],
-              hist.values()[ lineLen*lineNum : lineLen*(lineNum+1) ] )
+#    if lineNum*lineLen >= len(sortedData):
+#      break
+    for x in (xrange(5)):
+      index = lineLen*lineNum + x
+      if index >= len(sortedData):
+        print; return
+      theVal = format(str(sortedData[index][0]), str((valLen))+"s")
+      theKey = format(str(sortedData[index][1]), str((keyLen))+"s")
+      print "'%s':%s   " % (theKey,theVal),
+    print
     lineNum += 1
 
 try:
@@ -27,8 +45,9 @@ except IOError:
 
 hist = {}
 for char in text:
-  if not char.isalpha():
+  if char in string.whitespace:
     continue
   hist[char] = hist.get(char,0) + 1
 
-printDict(sorted(hist),5)
+printDict(hist,5)
+
